2ab8293a408d4446f1d516c71056a205ac66f145
algoritmika14 <117454825+algoritmika14@users.noreply.github.com>
2022-11-04 16:21:38 +0300

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World!");
+        print("Algoritmika");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
